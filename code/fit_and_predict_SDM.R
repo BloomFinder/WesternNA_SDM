5a318ebd1e1a9d0218b05c27fdc327b74f785455
ibreckhe <ibreckhe@gmail.com>
2018-01-11 10:48:00 -0800

    Initial Commit

---
+++ code/fit_and_predict_SDM.R
@@ -0,0 +1,230 @@
+####Fits SDMs to occupancy data.####
+library(sdm)
+library(readr)
+library(dplyr)
+library(foreach)
+library(doParallel)
+library(gdalUtils)
+proj_dir <- "~/WesternNA_SDM/"
+setwd(proj_dir)
+
+##Downloads test raster data from Amazon S3 if it doesn't already exist.
+if(!file.exists("./data/SDM_tiles_PNW.tar.gz")){
+  aws_dl1 <- "aws s3 cp s3://sdmdata/predictors/SDM_tiles_PNW.tar.gz ./data/SDM_tiles_PNW.tar.gz"
+  system(paste("cd",proj_dir,"&&",aws_dl1))
+  tar_dl1 <- "tar -xf ./data/SDM_tiles_PNW.tar.gz ./data/"
+  system(paste("cd",proj_dir,"&&",tar_dl1))
+}
+
+##Downloads full raster data from Amazon S3 if it doesn't already exist.
+# if(!file.exists("./data/SDM_tiles_WNA.tar.gz")){
+#   aws_dl2 <- "aws s3 cp s3://sdmdata/predictors/SDM_tiles_WNA.tar.gz ./data/SDM_tiles_WNA.tar.gz"
+#   system(paste("cd",proj_dir,"&&",aws_dl2),wait=TRUE)
+#   tar_dl2 <- "tar -xf ./data/SDM_tiles_WNA.tar.gz ./data/"
+#   system(paste("cd",proj_dir,"&&",tar_dl2),wait=TRUE)
+# }
+
+##Downloads point data from Amazon S3 if it doesn't already exist.
+if(!file.exists("./data/occurences_final_1_9_2018.tar.gz")){
+  aws_dl3 <- "aws s3 cp s3://sdmdata/occurences/occurences_final_1_9_2018.tar.gz ./data/occurences_final_1_9_2018.tar.gz"
+  system(paste("cd",proj_dir,"&&",aws_dl3),wait=TRUE)
+  tar_dl3 <- "tar -xf ./data/occurences_final_1_9_2018.tar.gz ./data/"
+  system(paste("cd",proj_dir,"&&",tar_dl3),wait=TRUE)
+}
+
+##Reads data in
+spd <- read_csv("./data/spocc_occurences_final_1_9_2017.csv")
+
+## Model fitting for focal species.
+set.seed(38)
+test_spp <- unique(spd$species)
+#test_spp <- sample(unique(spd$species),size=50,replace=FALSE)
+# test_spp <- c("Aconitum columbianum","Anaphalis margaritacea","Anemone occidentalis",
+#               "Balsamorhiza sagittata","Cassiope mertensiana","Erigeron peregrinus",
+#               "Luetkea pectinata","Senecio triangularis")
+
+cl <- makeCluster(12)
+registerDoParallel(cl)
+overwrite <- TRUE
+
+all_stats <- foreach(i=1:length(test_spp),.packages=c("dplyr","sdm"),
+                     .combine="rbind") %dopar% {
+                       
+                       setwd("proj_dir")
+                       
+                       fit_file <- paste("/home/rstudio/Dropbox/AMI_share/WNA_wildflower_SDM/scratch/sdm_fits/sdm_",gsub(" ","_",test_spp[i]),".Rdata",sep="")
+                       if(file.exists(fit_file) & overwrite==FALSE){
+                         cat(paste("Output file",paste(fit_file),"exists, skipping..."),
+                             file="./scratch/sdm_progress.log",append=TRUE)
+                       }else{
+                         cat(paste("Fitting model for ",test_spp[i],"(",i,"of",length(test_spp),") on",
+                                   Sys.time(),"\n"),file="./scratch/sdm_progress.log",append=TRUE)
+                         print(paste("Fitting model for ",test_spp[i],"(",i,"of",length(test_spp),")"))
+                         print(paste("Preparing data..."))
+                         tr_pres_abs <- spd
+                         tr_pres_abs$TR_PRES <- as.numeric(tr_pres_abs$species==test_spp[i])
+                         
+                         tr_pres <- as.data.frame(tr_pres_abs[tr_pres_abs$TR_PRES==1,c(1,2,17:ncol(tr_pres_abs))])
+                         
+                         tr_abs <- tr_pres_abs[tr_pres_abs$TR_PRES==0,c(1,2,17:ncol(tr_pres_abs))]
+                         tr_abs <- tr_abs[-which(tr_abs$loc %in% unique(tr_pres$loc)),]
+                         tr_abs <- as.data.frame(sample_n(tr_abs,size=max((nrow(tr_pres) * 2),4000)))
+                         
+                         tr_abs <- tr_abs[,-which(colnames(tr_abs)=="loc")]
+                         tr_pres <- tr_pres[,-which(colnames(tr_pres)=="loc")]
+                         tr_pres_abs2 <- rbind(tr_abs,tr_pres)
+                         
+                         ##Creates a test dataset which takes data from a subset of 90km blocks
+                         ##where a species is recorded as present, and another set where it is absent.
+                         blocks <- unique(tr_pres_abs2$location)
+                         blocks_pres <- unique(tr_pres_abs2$location[tr_pres_abs2$TR_PRES==1])
+                         blocks_abs <- blocks[!(blocks %in% blocks_pres)]
+                         n_valid_pblocks <- round(length(blocks_pres)*0.1)
+                         sample_pblocks <- sample(blocks_pres,size=n_valid_pblocks+1,replace=FALSE)
+                         n_valid_ablocks <- round(length(blocks_abs)*0.1)
+                         sample_ablocks <- sample(blocks_abs,size=n_valid_ablocks+1,replace=FALSE)
+                         valid_blocks <- c(sample_pblocks,sample_ablocks)
+                         
+                         test_data <- tr_pres_abs2[tr_pres_abs2$location %in% valid_blocks,-which(colnames(tr_pres_abs2) == "location")]
+                         train_data <- tr_pres_abs2[!(tr_pres_abs2$location %in% valid_blocks),-which(colnames(tr_pres_abs2) == "location")]
+                         all_data <- rbind(test_data,train_data)
+                         
+                         par(mfrow=c(1,2))
+                         plot(train_data$X,train_data$Y,col=train_data$TR_PRES + 1,
+                              main=paste(test_spp[i],"Training Data"))
+                         plot(test_data$X,test_data$Y,col=test_data$TR_PRES + 1,
+                              main=paste(test_spp[i],"Test Data"))
+                         
+                         
+                         tr_sdmd <- sdmData(TR_PRES ~ . + f(PCT_EFW) + f(PCT_ECO) + f(PSL_TUS) + f(PSL_TWL),
+                                            train=train_data,test=test_data)
+                         print(tr_sdmd)
+                         
+                         ##Fits the models with heldout data to measure performance.
+                         print(paste("Initial fitting..."))
+                         
+                         tr_sdm1 <- sdm(TR_PRES ~ PLC_TRE + PLC_HRB + PCM_CMD + PCM_TD + PCM_PAS + PCM_DD5 + PCM_MAP + PSL_BDR + PSL_SND + PSL_CAR + PSL_PHO + PCL_SE1 + PCL_SE2 + PCL_MRA + PSW_DIS + PTP_RLV + PTP_WET + PCO_XSC + PCO_YSC,
+                                        data=tr_sdmd,methods=c("glm","gbm","svm","maxent"),interaction.depth=2,
+                                        var.selection=FALSE)
+                         
+                         print(tr_sdm1)
+                         
+                         ##Extracts test data performance stats from fit models.
+                         glm_stats1 <- tr_sdm1@models$TR_PRES$glm$'1'@evaluation$test.indep@threshold_based
+                         glm_stats2 <- tr_sdm1@models$TR_PRES$glm$'1'@evaluation$test.indep@statistics
+                         gbm_stats1 <- tr_sdm1@models$TR_PRES$brt$'2'@evaluation$test.indep@threshold_based
+                         gbm_stats2 <- tr_sdm1@models$TR_PRES$brt$'2'@evaluation$test.indep@statistics
+                         svm_stats1 <- tr_sdm1@models$TR_PRES$svm$'3'@evaluation$training@threshold_based
+                         svm_stats2 <- tr_sdm1@models$TR_PRES$svm$'3'@evaluation$training@statistics
+                         mxt_stats1 <- tr_sdm1@models$TR_PRES$maxent$'4'@evaluation$test.indep@threshold_based
+                         mxt_stats2 <- tr_sdm1@models$TR_PRES$maxent$'4'@evaluation$test.indep@statistics
+                         tr_sdm_stats <- data.frame(rbind(c(glm_stats1,glm_stats2),
+                                                          c(gbm_stats1,gbm_stats2),
+                                                          c(svm_stats1,svm_stats2),
+                                                          c(mxt_stats1,mxt_stats2)))
+                         tr_sdm_stats$species <- test_spp[i]
+                         tr_sdm_stats$method <- c("glm","brt","svm","maxent")
+                         tr_sdm_stats$ensemble_weight <- (as.numeric(tr_sdm_stats$AUC)-0.5)/0.5
+                         
+                         ##Re-fits models with the full dataset.
+                         print(paste("Final fitting..."))
+                         tr_sdmd2 <- sdmData(TR_PRES ~ . + f(PCT_EFW) + f(PCT_ECO) + f(PSL_TUS) + f(PSL_TWL),
+                                             train=all_data)
+                         sdm_all <- sdm(TR_PRES ~ PLC_TRE + PLC_HRB + PCM_CMD + PCM_TD + PCM_PAS + PCM_DD5 + PCM_MAP + PSL_BDR + PSL_SND + PSL_CAR + PSL_PHO + PCL_SE1 + PCL_SE2 + PCL_MRA + PSW_DIS + PTP_RLV + PTP_WET + PCO_XSC + PCO_YSC,
+                                        data=tr_sdmd2,methods=c("glm","gbm","svm","maxent"),interaction.depth=2,
+                                        var.selection=FALSE)
+                         print(sdm_all)
+                         out <- list(list(data=tr_sdmd2,models=sdm_all,stats=tr_sdm_stats))
+                         names(out) <- test_spp[i]
+                         saveRDS(out,file=fit_file)
+                         cat(paste("Model object written to",fit_file,"on",
+                                   Sys.time(),"\n"),file="./scratch/sdm_progress.log",
+                             append=TRUE)
+                         (tr_sdm_stats)
+                       }
+                     }
+stopCluster(cl)
+AUC_mean <- mean(as.numeric(all_stats$AUC))
+AUC_mean
+all_stats_tbl <- as.tbl(all_stats)
+save(all_stats_tbl,file="./output/sdm_results.Rdata")
+
+####Creates spatial predictions using the weighted average of the models.####
+
+tile_path <- "~/WesternNA_SDM/data/SDM_tiles_PNW/"
+model_path <- "~/WesternNA_SDM/data/scratch/sdm_fits/"
+out_path <- "~/WesternNA_SDM/data/output/PNW_tiles/"
+mosaic_path <- "~/WesternNA_SDM/data/output/PNW_mosaic/"
+log_path <- "~/WesternNA_SDM/data/scratch/sdm_progress.log"
+
+pred_tiles <- list.files(tile_path, pattern=".tif$", full.names=TRUE)
+pred_names <- list.files(tile_path,pattern=".tif$", full.names=FALSE)
+
+model_files <- list.files(model_path,pattern=".Rdata",full.names=TRUE)
+overwrite=TRUE
+
+##Sets up cluster.
+cl <- makeCluster(38)
+registerDoParallel(cl)
+
+##Raster predictions.
+foreach(i=1:length(model_files),.packages=c("raster","sdm","gdalUtils")) %dopar% {
+  
+  ##Loads fit models.
+  out <- readRDS(model_files[i])
+  models <- out[[1]]$models
+  stats <- out[[1]]$stats
+  spp <- names(out)
+  remove(out)
+  
+  cat(paste("Raster predictions for",spp,"(",i,"of",length(model_files),") started on",
+            Sys.time(),"\n"),file=log_path,append=TRUE)
+  
+  ##Creates output directory for each species if it doesn't exist.
+  spp_dir <- paste(out_path,gsub(" ","_",spp),"/",sep="")
+  if(!dir.exists(spp_dir)){dir.create(spp_dir)}
+  
+  preds <- list()
+  for(j in 1:length(pred_tiles)){
+    outfile <- paste(spp_dir,"sdm_tile_",j,
+                     "_",gsub(" ","_",spp),".img",sep="")
+    if(file.exists(outfile) & overwrite==FALSE){
+      print(paste("File exists, skipping..."))
+    }else{
+      pred_tile <- brick(pred_tiles[j])
+      names(pred_tile) <- c('PCL_MAN', 'PCL_SE1', 'PCL_SE2', 'PCL_SE3', 'PCM_BFP',
+                            'PCM_CMD', 'PCM_DD5', 'PCM_MAP', 'PCM_PAS', 'PCM_TD',
+                            'PCT_ECO', 'PCT_EFW', 'PLC_HRB', 'PLC_TRE', 'PLC_URB',
+                            'PSL_BDR', 'PSL_CAR','PSL_PHO', 'PSL_SND', 'PSL_TUS',
+                            'PSL_TWL', 'PTP_ELV', 'PTP_RLV', 'PTP_SLP', 'PTP_WET',
+                            'PTP_ASP', 'PTP_SOL', 'PCL_MRA', 'PSW_DIS', 'PSW_OCC',
+                            'PCO_XSC', 'PCO_YSC')
+      pred <- try(ensemble(models,newdata=pred_tile,
+                           setting=list(method='weighted',
+                                        weights=stats$ensemble_weight[models@run.info$success]),
+                           filename=outfile,overwrite=TRUE,progress='text'))
+      preds[[i]] <- pred
+    }
+    
+  }
+  
+  ##Merges output tiles to single raster.
+  tiles <- list.files(spp_dir,pattern=".img$")
+  setwd(spp_dir)
+  mosaic_rasters(gdalfile=tiles,dst_dataset=paste(mosaic_path,gsub(" ","_",spp),"_mosaic.tif",sep=""),
+                 verbose=TRUE)
+  
+  ##Copies raster mosaic to S3 bucket.
+  cp_string <- paste("aws s3 cp",paste(mosaic_path,gsub(" ","_",spp),"_mosaic.tif",sep=""),
+                     paste("s3://sdmdata/PNW_mosaic/", gsub(" ","_",spp), "_mosaic.tif",sep=""))
+  system(cp_string,wait=TRUE)
+  
+  all_tile_files <- list.files(".")
+  rm_string <- paste("cd",spp_dir,"&&","rm",paste(all_tile_files, collapse=" "))
+  system(rm_string)
+  
+  cat(paste("Raster predictions for",spp,"(",i,"of",length(model_files),") completed on",
+            Sys.time(),"\n"),file=log_path,append=TRUE)
+}
+stopCluster(cl)
+
